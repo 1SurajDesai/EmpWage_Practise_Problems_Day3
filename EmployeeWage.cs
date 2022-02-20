94975d0401158933e33a2b4836e3ee16bf96a9d3
1SurajDesai <surajdesai219@gmail.com>
2022-02-20 15:26:43 +0530

    Adding program to calculate daily emplyee wage

---
+++ EmployeeWage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProgram
+{
+    public class EmpyeeWage
+    {
+        public static void CalculateEmpWage()
+        {
+            int num;
+            int EmpRatePerHr = 20, HrPerDay = 8, sallary = 0;
+            Random random = new Random();
+            num = random.Next(2);
+            if (num == 1)
+            {
+                sallary = EmpRatePerHr * HrPerDay;
+                Console.WriteLine("Sallary of employee is "+ sallary);
+
+            }
+            else
+            {
+                Console.WriteLine("Sallary of employee is " + sallary);
+
+            }
+        }
+    }
+}
