4dd549d01c6e83e2f12bc82c826ef88baa2089f1
1SurajDesai <surajdesai219@gmail.com>
2022-02-20 15:36:14 +0530

    Adding program to calculate wage using switch case

---
+++ WageUsingSwitchCase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProgram
+{
+    public class WageUsingSwitchCase
+    {
+        const int IsPartTime = 1;
+        const int IsFullTime = 2;
+        public static void CalWageSwitchCase()
+        {
+            
+            int EmpHr = 0, sallary = 0, EmpRatePerHr = 20;
+            Random random = new Random();
+            int EmpCheck = random.Next(3);
+            switch (EmpCheck)
+            {
+                case IsFullTime:
+                    EmpHr = 8;
+                    break;
+                case IsPartTime:
+                    EmpHr = 4;
+                    break;
+                default:
+                    EmpHr = 0;
+                    break;
+
+
+            }
+            sallary = EmpRatePerHr * EmpHr;
+            Console.WriteLine("Sallary of employee is" + " " + sallary);        }
+    }
+}
