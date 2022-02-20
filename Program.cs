d150811b5e0755b9d60f73e4a3fdb60ccdbe04ff
1SurajDesai <surajdesai219@gmail.com>
2022-02-20 15:54:14 +0530

    Updating Program.cs code

--- Program.cs
+++ Program.cs
@@ -5,13 +5,13 @@ namespace EmployeeWageProgram
     {
         public static void Main(string[] arg)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program ");
+            //Console.WriteLine("Welcome to Employee Wage Computation Program ");
             //EmployeeCheck.EmployeeAbsentOrPresent();
             //EmpyeeWage.CalculateEmpWage();
             //PartTimeEmpWage.CalEmpWage();
             //WageUsingSwitchCase.CalWageSwitchCase();
             //WageForMonth.CalWageOfMonth();
-            //CalWageTillCondMeet.CalWage();
+            CalWageTillCondMeet.CalWage();
 
         }
     }
