a87169c5145c1d11783e67dba8cec5e5fcec66ff
1SurajDesai <surajdesai219@gmail.com>
2022-02-20 15:32:29 +0530

    Adding program to calculate wage having part time

---
+++ PartTimeEmpWage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProgram
+{
+    public class PartTimeEmpWage
+    {
+        public static void CalEmpWage()
+        {
+            int IsPartTime = 1, IsFullTime = 2, EmpHr = 0, sallary = 0, EmpRatePerHr = 20;
+            Random random =  new Random();
+            int EmpCheck = random.Next(3);
+            if (EmpCheck == IsPartTime)
+            {
+                EmpHr = 4;
+            }
+            else if (EmpCheck == IsFullTime)
+            {
+                EmpHr = 8;
+            }
+            else
+            {
+                EmpHr = 0;
+            }
+            sallary = EmpHr * EmpRatePerHr;
+            Console.WriteLine("Sallary of employee is =" + sallary);
+        }
+    }
+}
