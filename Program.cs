0ea908520385927410ca69ce5d472bcbd4106ca5
1SurajDesai <surajdesai219@gmail.com>
2022-02-20 15:13:59 +0530

    Adding Welocome message program

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace EmployeeWageProgram
+{
+    public class Program
+    {
+        public static void Main(string[] arg)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program ");
+            //EmployeeCheck.EmployeeAbsentOrPresent();
+            //EmpyeeWage.CalculateEmpWage();
+            //PartTimeEmpWage.CalEmpWage();
+            //WageUsingSwitchCase.CalWageSwitchCase();
+            //WageForMonth.CalWageOfMonth();
+            //CalWageTillCondMeet.CalWage();
+
+        }
+    }
+}
